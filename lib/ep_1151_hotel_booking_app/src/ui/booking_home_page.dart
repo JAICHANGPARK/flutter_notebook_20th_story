048a7b1cab6300296fe420df3400cffe71c233af
Dreamwalker <aristojeff@gmail.com>
2022-05-13 21:17:25 +0900

    Create booking_home_page.dart

---
+++ lib/ep_1151_hotel_booking_app/src/ui/booking_home_page.dart
@@ -0,0 +1,19 @@
+
+
+import 'package:flutter/material.dart';
+
+class BookingHomePage extends StatefulWidget {
+  const BookingHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<BookingHomePage> createState() => _BookingHomePageState();
+}
+
+class _BookingHomePageState extends State<BookingHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
