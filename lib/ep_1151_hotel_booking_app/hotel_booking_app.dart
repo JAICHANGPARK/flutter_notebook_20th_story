2940e200a89c444782e17c486dc02b81ef5d837e
Dreamwalker <aristojeff@gmail.com>
2022-05-13 21:18:35 +0900

    Update hotel_booking_app.dart

--- lib/ep_1151_hotel_booking_app/hotel_booking_app.dart
+++ lib/ep_1151_hotel_booking_app/hotel_booking_app.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'src/ui/booking_home_page.dart';
+
 class HotelBookingApp extends StatelessWidget {
   const HotelBookingApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      routes: {
+        "/": (context) => BookingHomePage(),
+      },
+    );
   }
 }
