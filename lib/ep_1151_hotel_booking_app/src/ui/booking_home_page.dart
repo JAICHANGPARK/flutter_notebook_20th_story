f7af44d88b87e3421f4345142dc577c726bf50e4
Dreamwalker <aristojeff@gmail.com>
2022-05-13 21:22:25 +0900

    Update booking_home_page.dart

--- lib/ep_1151_hotel_booking_app/src/ui/booking_home_page.dart
+++ lib/ep_1151_hotel_booking_app/src/ui/booking_home_page.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 
 class BookingHomePage extends StatefulWidget {
@@ -13,7 +11,48 @@ class _BookingHomePageState extends State<BookingHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned(
+              left: 16,
+              right: 0,
+              bottom: 0,
+              top: 24,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Hello"),
+                  Text("Let's find the best"),
+                  Text("hotel for you"),
 
+                  SizedBox(
+                    height: 320,
+                    child: Card(
+                      child: Center(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                height: 72,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(24),
+                    topRight: Radius.circular(24),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
