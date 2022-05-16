9ae99e5035fdaeafeeeaf178295b4b32bf7d2c74
Dreamwalker <aristojeff@gmail.com>
2022-05-16 19:52:56 +0900

    Update housing_app.dart

--- lib/ep_1154_housing_app/housing_app.dart
+++ lib/ep_1154_housing_app/housing_app.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-
+import './src/ui/housing_home_page.dart';
 class HousingApp extends StatelessWidget {
   const HousingApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+        home: HousingHomePage(),
     );
   }
 }
