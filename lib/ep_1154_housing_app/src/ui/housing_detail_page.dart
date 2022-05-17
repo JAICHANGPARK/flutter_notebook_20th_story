12dcf2197fbefef9dee979ecca5a8fcbcc187ef2
Dreamwalker <aristojeff@gmail.com>
2022-05-17 19:43:56 +0900

    Create housing_detail_page.dart

---
+++ lib/ep_1154_housing_app/src/ui/housing_detail_page.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class HousingDetailPage extends StatefulWidget {
+  const HousingDetailPage({Key? key}) : super(key: key);
+
+  @override
+  State<HousingDetailPage> createState() => _HousingDetailPageState();
+}
+
+class _HousingDetailPageState extends State<HousingDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
