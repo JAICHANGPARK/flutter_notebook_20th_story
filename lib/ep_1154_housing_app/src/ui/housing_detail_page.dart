64d0316848dd773c277874e7a6318c5a5d40e352
Dreamwalker <aristojeff@gmail.com>
2022-05-17 19:49:55 +0900

    Update housing_detail_page.dart

--- lib/ep_1154_housing_app/src/ui/housing_detail_page.dart
+++ lib/ep_1154_housing_app/src/ui/housing_detail_page.dart
@@ -17,23 +17,67 @@ class _HousingDetailPageState extends State<HousingDetailPage> {
             Positioned(
               left: 16,
               top: 16,
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.grey,
+              child: Column(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey,
+                      ),
+                      shape: BoxShape.circle,
+                    ),
+                    padding: EdgeInsets.all(8),
+                    child: Center(
+                      child: Icon(
+                        Icons.arrow_back_ios,
+                        size: 16,
+                      ),
+                    ),
                   ),
-                  shape: BoxShape.circle,
-                ),
-                padding: EdgeInsets.all(8),
-                child: Center(
-                  child: Icon(
-                    Icons.arrow_back_ios,
-                    size: 16,
+                  SizedBox(
+                    height: 48,
                   ),
-                ),
+                  Row(
+                    children: [
+                      CircleAvatar(),
+                      Column(
+                        children: [
+                          Text("Bedroom"),
+                          Text("6 bedroom")
+                        ],
+                      )
+                    ],
+                  )
+                ],
               ),
             ),
-            
+            Positioned(
+              child: Container(
+                color: Colors.grey,
+                height: MediaQuery.of(context).size.height / 2,
+              ),
+              left: 16,
+              bottom: 0,
+              right: 0,
+            ),
+            Positioned(
+              child: Container(
+                color: Colors.blue,
+                width: 180,
+                height: MediaQuery.of(context).size.height / 2,
+              ),
+              top: 0,
+              right: 0,
+            ),
+            Positioned(
+              child: Container(
+                color: Colors.red,
+                width: 180,
+                height: 48,
+              ),
+              bottom: MediaQuery.of(context).size.height / 2.5,
+              right: 0,
+            )
           ],
         ),
       ),
