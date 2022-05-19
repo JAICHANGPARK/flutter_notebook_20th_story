18d7e4fddce0cc66cb2e3b3b9df010cb4dc142a6
Dreamwalker <aristojeff@gmail.com>
2022-05-19 19:34:25 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 
 
 import 'package:flutter/material.dart';
-// import 'package:flutter_notebook_20th_story/ep_1151_hotel_booking_app/hotel_booking_app.dart';
-import 'package:flutter_notebook_20th_story/ep_1154_housing_app/housing_app.dart';
 
+import 'package:flutter_notebook_20th_story/ep_1154_housing_app/housing_app.dart';
+import 'package:flutter_notebook_20th_story/ep_1157_delivery_app/delivery_app.dart';
 main() {
-  runApp(HousingApp());
+  runApp(DeliveryApp());
 }
