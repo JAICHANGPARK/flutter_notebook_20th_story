7db329245c98a0c5fd893aec475be151823089ee
Dreamwalker <aristojeff@gmail.com>
2022-05-19 19:33:30 +0900

    Create delivery_app.dart

---
+++ lib/ep_1157_delivery_app/delivery_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DeliveryApp extends StatefulWidget {
+  const DeliveryApp({Key key}) : super(key: key);
+
+  @override
+  _DeliveryAppState createState() => _DeliveryAppState();
+}
+
+class _DeliveryAppState extends State<DeliveryApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
