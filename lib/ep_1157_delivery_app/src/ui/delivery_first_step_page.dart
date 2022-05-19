548c989a2e60fd59ba35e2156657a5607a5753a1
Dreamwalker <aristojeff@gmail.com>
2022-05-19 19:41:18 +0900

    Update delivery_first_step_page.dart

--- lib/ep_1157_delivery_app/src/ui/delivery_first_step_page.dart
+++ lib/ep_1157_delivery_app/src/ui/delivery_first_step_page.dart
@@ -7,15 +7,60 @@ class DeliveryFirstStepPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              child: Icon(
-                Icons.arrow_back_outlined,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black,
+                  ),
+                  borderRadius: BorderRadius.circular(4),
+                  color: Color.fromRGBO(192, 191, 248, 1),
+                ),
+                padding: EdgeInsets.all(8),
+                child: Icon(
+                  Icons.arrow_back_outlined,
+                ),
               ),
-            )
-          ],
+              SizedBox(
+                height: 24,
+              ),
+              Text(
+                "Destinations",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 160,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      left: 0,
+                      top: 8,
+                      right: 0,
+                      bottom: 0,
+                      child: Container(
+                        color: Colors.orange,
+                      ),
+                    ),
+                    Positioned(
+                      left: 0,
+                      top: 0,
+                      right: 8,
+                      bottom: 8,
+                      child: Container(
+                        color: Colors.pink,
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
