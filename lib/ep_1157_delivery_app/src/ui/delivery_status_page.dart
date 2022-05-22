bf476fe6b8b07265491b12aedc24fce86adc655a
Dreamwalker <aristojeff@gmail.com>
2022-05-22 15:55:33 +0900

    Create delivery_status_page.dart

---
+++ lib/ep_1157_delivery_app/src/ui/delivery_status_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:dotted_border/dotted_border.dart';
+import 'package:dotted_line/dotted_line.dart';
+
+class DeliveryStatusPage extends StatefulWidget {
+  const DeliveryStatusPage({Key? key}) : super(key: key);
+
+  @override
+  State<DeliveryStatusPage> createState() => _DeliveryStatusPageState();
+}
+
+class _DeliveryStatusPageState extends State<DeliveryStatusPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
