ce8bf28a7853b9866e3713f99b279e94c7b9990d
Dreamwalker <aristojeff@gmail.com>
2022-05-23 19:17:45 +0900

    Update delivery_app.dart

--- lib/ep1157_delivery_app/delivery_app.dart
+++ lib/ep1157_delivery_app/delivery_app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_20th_story/ep_1157_delivery_app/src/ui/delivery_first_step_page.dart';
+import 'package:flutter_notebook_20th_story/ep1157_delivery_app/src/ui/delivery_first_step_page.dart';
 class DeliveryApp extends StatelessWidget {
   const DeliveryApp({Key? key}) : super(key: key);
 
