a5a0b27ba453c159d86046d8c8e7a26b43260370
Dreamwalker <aristojeff@gmail.com>
2022-05-23 19:17:13 +0900

    update file name

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,7 @@
 
 import 'package:flutter/material.dart';
 
-import 'package:flutter_notebook_20th_story/ep_1154_housing_app/housing_app.dart';
-import 'package:flutter_notebook_20th_story/ep_1157_delivery_app/delivery_app.dart';
+
 main() {
   runApp(DeliveryApp());
 }
