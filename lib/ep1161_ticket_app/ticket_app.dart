d9287478ad360b58f20e15dffefae5346e0507e9
Dreamwalker <aristojeff@gmail.com>
2022-05-23 19:21:22 +0900

    Update ticket_app.dart

--- lib/ep1161_ticket_app/ticket_app.dart
+++ lib/ep1161_ticket_app/ticket_app.dart
@@ -1,13 +1,14 @@
 
 
 import 'package:flutter/material.dart';
-
+import './src/ui/ticket_page.dart';
 class TicketApp extends StatelessWidget {
   const TicketApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: TicketPage(),
 
     );
   }
