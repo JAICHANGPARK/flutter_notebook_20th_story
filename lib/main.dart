c96a1dfc6a40e9097c7a3d1b8d8b38ede069dbdb
Dreamwalker <aristojeff@gmail.com>
2022-05-23 19:18:46 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 
 
 import 'package:flutter/material.dart';
-
+import 'package:flutter_notebook_20th_story/ep1161_ticket_app/ticket_app.dart';
 
 main() {
-  runApp(DeliveryApp());
+  runApp(TicketApp());
 }
