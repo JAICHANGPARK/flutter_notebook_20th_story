a8a46ae3d348e7dfc46c84120bea94200fe47a51
Dreamwalker <aristojeff@gmail.com>
2022-05-23 19:18:49 +0900

    Create ticket_app.dart

---
+++ lib/ep1161_ticket_app/ticket_app.dart
@@ -0,0 +1,14 @@
+
+
+import 'package:flutter/material.dart';
+
+class TicketApp extends StatelessWidget {
+  const TicketApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
