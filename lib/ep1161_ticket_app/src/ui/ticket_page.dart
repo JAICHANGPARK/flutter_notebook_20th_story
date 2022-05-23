d7c2a746a3d02b7ef2537a9b7b9fc1c786e1e5c5
Dreamwalker <aristojeff@gmail.com>
2022-05-23 19:21:19 +0900

    Create ticket_page.dart

---
+++ lib/ep1161_ticket_app/src/ui/ticket_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TicketPage extends StatelessWidget {
+  const TicketPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
