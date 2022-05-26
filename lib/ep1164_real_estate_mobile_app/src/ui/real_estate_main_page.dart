83e95b0dc9cf888904b453faacd70944a2d16f42
Dreamwalker <aristojeff@gmail.com>
2022-05-26 19:37:22 +0900

    Update real_estate_main_page.dart

--- lib/ep1164_real_estate_mobile_app/src/ui/real_estate_main_page.dart
+++ lib/ep1164_real_estate_mobile_app/src/ui/real_estate_main_page.dart
@@ -17,13 +17,16 @@ class _RealEstateMainPageState extends State<RealEstateMainPage> {
         index: _index,
         children: [
           Positioned(
-              child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: NetworkImage(""),
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage("https://cdn.pixabay.com/photo/"
+                      "2017/03/30/04/14/house-2187170_960_720.jpg"),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ))
+          )
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
