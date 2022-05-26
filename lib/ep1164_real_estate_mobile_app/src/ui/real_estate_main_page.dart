48f1f913460a5eb0fec0a9fcd9586215ac0843a3
Dreamwalker <aristojeff@gmail.com>
2022-05-26 19:28:18 +0900

    Create real_estate_main_page.dart

---
+++ lib/ep1164_real_estate_mobile_app/src/ui/real_estate_main_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+
+class RealEstateMainPage extends StatefulWidget {
+  const RealEstateMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<RealEstateMainPage> createState() => _RealEstateMainPageState();
+}
+
+class _RealEstateMainPageState extends State<RealEstateMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
