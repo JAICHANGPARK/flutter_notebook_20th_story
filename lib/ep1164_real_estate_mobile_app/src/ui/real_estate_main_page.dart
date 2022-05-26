182047c6828de15f5837d8556b3e142a328a73f8
Dreamwalker <aristojeff@gmail.com>
2022-05-26 19:34:41 +0900

    Update real_estate_main_page.dart

--- lib/ep1164_real_estate_mobile_app/src/ui/real_estate_main_page.dart
+++ lib/ep1164_real_estate_mobile_app/src/ui/real_estate_main_page.dart
@@ -8,34 +8,23 @@ class RealEstateMainPage extends StatefulWidget {
 }
 
 class _RealEstateMainPageState extends State<RealEstateMainPage> {
+  int _index = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         selectedItemColor: Colors.black,
-
+        currentIndex: _index,
+        onTap: (idx){
+          setState(()=> _index = idx);
+        },
         items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_filled),
-            label: "Home"
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.language),
-              label: "Explore"
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.bookmarks),
-              label: "Save"
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.shopping_bag),
-              label: "My Trip"
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.account_circle),
-              label: "Profile"
-          ),
+          BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.language), label: "Explore"),
+          BottomNavigationBarItem(icon: Icon(Icons.bookmarks), label: "Save"),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_bag), label: "My Trip"),
+          BottomNavigationBarItem(icon: Icon(Icons.account_circle), label: "Profile"),
         ],
       ),
     );
