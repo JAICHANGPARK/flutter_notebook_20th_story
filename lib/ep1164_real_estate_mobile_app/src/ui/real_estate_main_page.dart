4ae386148beea2c68e2b46e9f98b23536698f3a9
Dreamwalker <aristojeff@gmail.com>
2022-05-27 21:04:13 +0900

    Update real_estate_main_page.dart

--- lib/ep1164_real_estate_mobile_app/src/ui/real_estate_main_page.dart
+++ lib/ep1164_real_estate_mobile_app/src/ui/real_estate_main_page.dart
@@ -95,11 +95,26 @@ class _RealEstateMainPageState extends State<RealEstateMainPage> {
           setState(() => _index = idx);
         },
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.language), label: "Explore"),
-          BottomNavigationBarItem(icon: Icon(Icons.bookmarks), label: "Save"),
-          BottomNavigationBarItem(icon: Icon(Icons.shopping_bag), label: "My Trip"),
-          BottomNavigationBarItem(icon: Icon(Icons.account_circle), label: "Profile"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_filled),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.language),
+            label: "Explore",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bookmarks),
+            label: "Save",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_bag),
+            label: "My Trip",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: "Profile",
+          ),
         ],
       ),
     );
