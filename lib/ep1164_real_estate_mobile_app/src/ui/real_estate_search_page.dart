134790bc2ce331928c9a27dc4e8c3563b1c2a82a
Dreamwalker <aristojeff@gmail.com>
2022-05-28 19:06:14 +0900

    Create real_estate_search_page.dart

---
+++ lib/ep1164_real_estate_mobile_app/src/ui/real_estate_search_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class RealEstateSearchPage extends StatefulWidget {
+  const RealEstateSearchPage({Key? key}) : super(key: key);
+
+  @override
+  State<RealEstateSearchPage> createState() => _RealEstateSearchPageState();
+}
+
+class _RealEstateSearchPageState extends State<RealEstateSearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
