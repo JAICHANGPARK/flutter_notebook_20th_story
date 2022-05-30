2b48d2269e03c3cc34d1309249e4743ee598f9d2
Dreamwalker <aristojeff@gmail.com>
2022-05-30 19:45:51 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-
-
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_20th_story/ep1164_real_estate_mobile_app/real_estate_mobile_app.dart';
+import 'package:flutter_notebook_20th_story/ep1168_file_manager/file_manager_app.dart';
 
 main() {
-  runApp(RealEstateMobileApp());
+  runApp(FileManagerApp());
 }
