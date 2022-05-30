ac5503ba9da15a096ef327a4eb3e17f332f9651b
Dreamwalker <aristojeff@gmail.com>
2022-05-30 19:45:56 +0900

    Update file_manager_app.dart

---
+++ lib/ep1168_file_manager/file_manager_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class FileManagerApp extends StatelessWidget {
+  const FileManagerApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
