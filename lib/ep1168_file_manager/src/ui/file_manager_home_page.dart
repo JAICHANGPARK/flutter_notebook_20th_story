cc0bdc96c1837e188b1d5cff582d677bab3b7616
Dreamwalker <aristojeff@gmail.com>
2022-05-30 19:51:03 +0900

    Update file_manager_home_page.dart

--- lib/ep1168_file_manager/src/ui/file_manager_home_page.dart
+++ lib/ep1168_file_manager/src/ui/file_manager_home_page.dart
@@ -11,8 +11,13 @@ class _FileManagerHomePageState extends State<FileManagerHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromRGBO(23, 24, 34, 1),
       body: Column(),
       bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.tealAccent,
+        onTap: (idx) {},
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Color.fromRGBO(38, 44, 73, 1),
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.link_rounded),
