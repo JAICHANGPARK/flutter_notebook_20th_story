f40c85fbaa1a75c74e471c660bebdd060040063b
Dreamwalker <aristojeff@gmail.com>
2022-06-01 18:20:04 +0900

    Update file_manager_home_page.dart

--- lib/ep1168_file_manager/src/ui/file_manager_home_page.dart
+++ lib/ep1168_file_manager/src/ui/file_manager_home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'widgets/file_manager_card_style_max_widget.dart';
-import 'widgets/fm_card_normal_widget.dart';
+import 'fm_link_page.dart';
 
 class FileManagerHomePage extends StatefulWidget {
   const FileManagerHomePage({Key? key}) : super(key: key);
@@ -66,52 +65,18 @@ class _FileManagerHomePageState extends State<FileManagerHomePage> {
           Expanded(
             child: IndexedStack(
               index: _btmIndex,
-
               children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                  child: Container(
-                    height: 42,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.white,
-                      ),
-                      borderRadius: BorderRadius.circular(4),
-                    ),
-                    padding: EdgeInsets.symmetric(horizontal: 16),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Search",
-                        hintStyle: TextStyle(
-                          color: Colors.grey,
-                        ),
-                        icon: Icon(
-                          Icons.search,
-                          color: Colors.grey,
-                        ),
-                        iconColor: Colors.grey,
-                        suffixIcon: Icon(
-                          Icons.sync_alt,
-                          color: Colors.grey,
-                        ),
-                        suffixIconColor: Colors.grey,
-                      ),
-                    ),
-                  ),
-                ),
                 Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(16, 8, 16, 0),
-                    child: ListView(
-                      padding: EdgeInsets.zero,
-                      children: [
-                        FMCardMaxWidget(),
-                        ...List.generate(100, (index) => FMCardNormalWidget()),
-                      ],
+                  child: FMLinkPage(),
+                ),
+                Center(
+                  child: Text(
+                    "Private",
+                    style: TextStyle(
+                      color: Colors.tealAccent,
                     ),
                   ),
-                ),
+                )
               ],
             ),
           ),
