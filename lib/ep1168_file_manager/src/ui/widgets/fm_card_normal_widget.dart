1335a1e5d5995d0d8f063535de6fd8a800d3bc20
Dreamwalker <aristojeff@gmail.com>
2022-06-01 18:10:22 +0900

    Create fm_card_normal_widget.dart

---
+++ lib/ep1168_file_manager/src/ui/widgets/fm_card_normal_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class FMCardNormalWidget extends StatelessWidget {
+  const FMCardNormalWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
