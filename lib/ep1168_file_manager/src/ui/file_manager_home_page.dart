54cee2b8eb4c0cf0f0048cb810a5d5bc0bfdaf1a
Dreamwalker <aristojeff@gmail.com>
2022-06-01 18:17:14 +0900

    Update file_manager_home_page.dart

--- lib/ep1168_file_manager/src/ui/file_manager_home_page.dart
+++ lib/ep1168_file_manager/src/ui/file_manager_home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'widgets/file_manager_card_style_max_widget.dart';
 import 'widgets/fm_card_normal_widget.dart';
+
 class FileManagerHomePage extends StatefulWidget {
   const FileManagerHomePage({Key? key}) : super(key: key);
 
@@ -62,48 +63,56 @@ class _FileManagerHomePageState extends State<FileManagerHomePage> {
           SizedBox(
             height: 16,
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: Container(
-              height: 42,
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.white,
-                ),
-                borderRadius: BorderRadius.circular(4),
-              ),
-              padding: EdgeInsets.symmetric(horizontal: 16),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: "Search",
-                  hintStyle: TextStyle(
-                    color: Colors.grey,
-                  ),
-                  icon: Icon(
-                    Icons.search,
-                    color: Colors.grey,
-                  ),
-                  iconColor: Colors.grey,
-                  suffixIcon: Icon(
-                    Icons.sync_alt,
-                    color: Colors.grey,
-                  ),
-                  suffixIconColor: Colors.grey,
-                ),
-              ),
-            ),
-          ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(16, 8, 16, 0),
-              child: ListView(
-                padding: EdgeInsets.zero,
-                children: [
-                  FMCardMaxWidget(),
-                  ...List.generate(100, (index) => FMCardNormalWidget()),
-                ],
-              ),
+            child: IndexedStack(
+              index: _btmIndex,
+
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: Container(
+                    height: 42,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.white,
+                      ),
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Search",
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
+                        ),
+                        icon: Icon(
+                          Icons.search,
+                          color: Colors.grey,
+                        ),
+                        iconColor: Colors.grey,
+                        suffixIcon: Icon(
+                          Icons.sync_alt,
+                          color: Colors.grey,
+                        ),
+                        suffixIconColor: Colors.grey,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(16, 8, 16, 0),
+                    child: ListView(
+                      padding: EdgeInsets.zero,
+                      children: [
+                        FMCardMaxWidget(),
+                        ...List.generate(100, (index) => FMCardNormalWidget()),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
