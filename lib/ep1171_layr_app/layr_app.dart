e78d901628923a6c965097d91937f6381453c3af
Dreamwalker <aristojeff@gmail.com>
2022-06-02 20:16:47 +0900

    Create layr_app.dart

---
+++ lib/ep1171_layr_app/layr_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class LayrApp extends StatelessWidget {
+  const LayrApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
