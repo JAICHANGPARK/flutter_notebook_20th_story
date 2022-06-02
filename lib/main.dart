21d7342190a84096bb71aba207bb2f42d8a2c328
Dreamwalker <aristojeff@gmail.com>
2022-06-02 20:19:05 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_20th_story/ep1168_file_manager/file_manager_app.dart';
-
+import 'package:flutter_notebook_20th_story/ep1171_layr_app/layr_app.dart';
 main() {
-  runApp(FileManagerApp());
+  runApp(LayrApp());
 }
