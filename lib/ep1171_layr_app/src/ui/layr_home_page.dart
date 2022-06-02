391ca02b5eaac6cd3392c872751fca8456be0cc7
Dreamwalker <aristojeff@gmail.com>
2022-06-02 20:23:23 +0900

    Update layr_home_page.dart

--- lib/ep1171_layr_app/src/ui/layr_home_page.dart
+++ lib/ep1171_layr_app/src/ui/layr_home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final layrAppBottomIndex = StateProvider((ref) => 0);
 
 class LayrHomePage extends StatefulWidget {
   const LayrHomePage({Key? key}) : super(key: key);
