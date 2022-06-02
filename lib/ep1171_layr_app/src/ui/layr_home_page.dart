fb41ed90f8522b4753d5d8ecc4b5ffae400e24e0
Dreamwalker <aristojeff@gmail.com>
2022-06-02 20:18:01 +0900

    Create layr_home_page.dart

---
+++ lib/ep1171_layr_app/src/ui/layr_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class LayrHomePage extends StatefulWidget {
+  const LayrHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<LayrHomePage> createState() => _LayrHomePageState();
+}
+
+class _LayrHomePageState extends State<LayrHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
