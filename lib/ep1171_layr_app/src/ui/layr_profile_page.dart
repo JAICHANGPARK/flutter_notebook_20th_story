2b9a2ede133d22b788ab0296c3a00b518ab87764
Dreamwalker <aristojeff@gmail.com>
2022-06-03 19:58:51 +0900

    Update layr_profile_page.dart

--- lib/ep1171_layr_app/src/ui/layr_profile_page.dart
+++ lib/ep1171_layr_app/src/ui/layr_profile_page.dart
@@ -14,12 +14,33 @@ class _LayrProfilePageState extends State<LayrProfilePage> {
       length: 2,
       child: Column(
         children: [
+          SizedBox(height: 42,
+            child: Row(
+              children: [
+                Expanded(child: Container()),
+                Expanded(
+                    child: Center(
+                  child: Icon(
+                    Icons.playlist_add,
+                  ),
+                )),
+                Expanded(child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Icon(Icons.notifications_active_outlined))),
+              ],
+            ),
+          ),
+          Divider(
+            height: 0,
+            color: Colors.grey,
+          ),
           SizedBox(
             height: MediaQuery.of(context).size.height / 2.2,
             child: Stack(
               children: [
-                Positioned(child: IconButton(
-                  onPressed: (){},
+                Positioned(
+                    child: IconButton(
+                  onPressed: () {},
                   icon: Icon(Icons.more_horiz),
                   color: Colors.pink,
                 ))
