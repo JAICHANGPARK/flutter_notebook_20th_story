27a696c32572f9db6861ed16480ebc8eedac440b
Dreamwalker <aristojeff@gmail.com>
2022-06-03 19:54:02 +0900

    Update layr_profile_page.dart

--- lib/ep1171_layr_app/src/ui/layr_profile_page.dart
+++ lib/ep1171_layr_app/src/ui/layr_profile_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class LayrProfilePage extends StatefulWidget {
   const LayrProfilePage({Key? key}) : super(key: key);
 
@@ -11,13 +10,26 @@ class LayrProfilePage extends StatefulWidget {
 class _LayrProfilePageState extends State<LayrProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-
-        SizedBox(height: MediaQuery.of(context).size.height / 2,),
-        Expanded(child: Placeholder())
-
-      ],
+    return DefaultTabController(
+      length: 2,
+      child: Column(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height / 2.2,
+          ),
+          TabBar(tabs: []),
+          Divider(
+            height: 0,
+            color: Colors.grey,
+          ),
+          Expanded(child: TabBarView(
+            children: [
+              Container(),
+              Container(),
+            ],
+          ))
+        ],
+      ),
     );
   }
 }
