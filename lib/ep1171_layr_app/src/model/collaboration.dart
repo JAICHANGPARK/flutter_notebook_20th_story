6a58717a82cbde1cb6650a876be9714c2699f74f
Dreamwalker <aristojeff@gmail.com>
2022-06-04 17:38:26 +0900

    Update collaboration.dart

--- lib/ep1171_layr_app/src/model/collaboration.dart
+++ lib/ep1171_layr_app/src/model/collaboration.dart
@@ -1,5 +1,13 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'collaboration.freezed.dart';
 
 @freezed
-class Collaboration with _$Collaboration{
-
+class Collaboration with _$Collaboration {
+  factory Collaboration({
+    String? title,
+    String? duration,
+    String? datatime,
+    List<String>? profileImages,
+  }) = _Collaboration;
 }
