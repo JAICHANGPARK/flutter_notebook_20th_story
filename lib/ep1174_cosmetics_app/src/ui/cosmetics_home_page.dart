f3b99cfc10cb9ffd1ee416684af1877c7b179961
Dreamwalker <aristojeff@gmail.com>
2022-06-05 17:26:13 +0900

    Create cosmetics_home_page.dart

---
+++ lib/ep1174_cosmetics_app/src/ui/cosmetics_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CosmeticsHomePage extends StatefulWidget {
+  const CosmeticsHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<CosmeticsHomePage> createState() => _CosmeticsHomePageState();
+}
+
+class _CosmeticsHomePageState extends State<CosmeticsHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
