e1194811c5603b8be25876c3e95919138d9f29c5
Dreamwalker <aristojeff@gmail.com>
2022-06-05 17:27:07 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_20th_story/ep1168_file_manager/file_manager_app.dart';
-import 'package:flutter_notebook_20th_story/ep1171_layr_app/layr_app.dart';
+import 'package:flutter_notebook_20th_story/ep1174_cosmetics_app/cosmetics_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 main() {
   runApp(ProviderScope(
-    child: LayrApp(),
+    child: CosmeticsApp(),
   ));
 }
