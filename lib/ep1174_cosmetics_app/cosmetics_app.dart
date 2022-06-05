810e570ee77add7d1e80fd22fb0310454ce519c1
Dreamwalker <aristojeff@gmail.com>
2022-06-05 17:25:36 +0900

    Update cosmetics_app.dart

---
+++ lib/ep1174_cosmetics_app/cosmetics_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class CosmeticsApp extends StatelessWidget {
+  const CosmeticsApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
