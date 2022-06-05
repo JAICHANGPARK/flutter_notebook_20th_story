bfd55b9776c0e8cf6904b45086da95d6dc4e4210
Dreamwalker <aristojeff@gmail.com>
2022-06-05 17:26:50 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_notebook_20th_story/ep1168_file_manager/file_manager_app.dart';
 import 'package:flutter_notebook_20th_story/ep1171_layr_app/layr_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-main() {
 
-  runApp(
-      ProviderScope(
-        child: LayrApp(),
-      )
-      );
+main() {
+  runApp(ProviderScope(
+    child: LayrApp(),
+  ));
 }
