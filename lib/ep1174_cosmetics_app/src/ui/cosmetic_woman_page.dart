59be7017657a9640312c4362770c9fc7a9ee4db7
Dreamwalker <aristojeff@gmail.com>
2022-06-06 15:04:17 +0900

    Update cosmetic_woman_page.dart

--- lib/ep1174_cosmetics_app/src/ui/cosmetic_woman_page.dart
+++ lib/ep1174_cosmetics_app/src/ui/cosmetic_woman_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class CosmeticWomanPage extends StatefulWidget {
   const CosmeticWomanPage({Key? key}) : super(key: key);
 
@@ -11,10 +10,24 @@ class CosmeticWomanPage extends StatefulWidget {
 class _CosmeticWomanPageState extends State<CosmeticWomanPage> {
   @override
   Widget build(BuildContext context) {
-    return    SingleChildScrollView(
+    return SingleChildScrollView(
       child: Column(
         children: [
-
+          Row(
+            children: [
+              Text(
+                "Popular",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text("See all"),
+              )
+            ],
+          )
         ],
       ),
     );
