a9f4745c0ab55193c5366688b2d2e61ffa60e42a
Dreamwalker <aristojeff@gmail.com>
2022-06-06 15:02:56 +0900

    Update cosmetic_woman_page.dart

---
+++ lib/ep1174_cosmetics_app/src/ui/cosmetic_woman_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class CosmeticWomanPage extends StatefulWidget {
+  const CosmeticWomanPage({Key? key}) : super(key: key);
+
+  @override
+  State<CosmeticWomanPage> createState() => _CosmeticWomanPageState();
+}
+
+class _CosmeticWomanPageState extends State<CosmeticWomanPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
