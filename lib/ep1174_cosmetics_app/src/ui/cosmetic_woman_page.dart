34158199e442cce2e9582c82be9e751f147dfb5d
Dreamwalker <aristojeff@gmail.com>
2022-06-06 15:11:07 +0900

    Update cosmetic_woman_page.dart

--- lib/ep1174_cosmetics_app/src/ui/cosmetic_woman_page.dart
+++ lib/ep1174_cosmetics_app/src/ui/cosmetic_woman_page.dart
@@ -35,36 +35,39 @@ class _CosmeticWomanPageState extends State<CosmeticWomanPage> {
               ],
             ),
           ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height / 2.5,
-            child: Container(
-              color: Colors.red,
-              child: ListView.builder(
-                itemCount: 4,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  return SizedBox(
-                    width: 160,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Expanded(child: Placeholder()),
-                        Text("Promio body lotion"),
-                        Row(
-                          children: [
-                            Text("\$29.99"),
-                            Text(
-                              "\$44.99",
-                              style: TextStyle(
-                                decoration: TextDecoration.lineThrough,
-                              ),
-                            )
-                          ],
-                        )
-                      ],
-                    ),
-                  );
-                },
+          Padding(
+            padding: const EdgeInsets.only(left: 16, bottom: 16),
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height / 2.5,
+              child: Container(
+                color: Colors.red,
+                child: ListView.builder(
+                  itemCount: 4,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return SizedBox(
+                      width: 160,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Expanded(child: Placeholder()),
+                          Text("Promio body lotion"),
+                          Row(
+                            children: [
+                              Text("\$29.99"),
+                              Text(
+                                "\$44.99",
+                                style: TextStyle(
+                                  decoration: TextDecoration.lineThrough,
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                ),
               ),
             ),
           )
