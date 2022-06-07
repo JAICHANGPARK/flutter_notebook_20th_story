df24ad03d4100f66b320166a031f8f2e0b2e3a4e
Dreamwalker <aristojeff@gmail.com>
2022-06-07 19:50:38 +0900

    Update cosmetics_cart_page.dart

---
+++ lib/ep1174_cosmetics_app/src/ui/cosmetics_cart_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class CosmeticsCartPage extends StatefulWidget {
+  const CosmeticsCartPage({Key? key}) : super(key: key);
+
+  @override
+  State<CosmeticsCartPage> createState() => _CosmeticsCartPageState();
+}
+
+class _CosmeticsCartPageState extends State<CosmeticsCartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
