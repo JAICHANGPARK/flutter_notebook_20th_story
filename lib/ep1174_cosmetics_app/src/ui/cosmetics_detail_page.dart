b4f29f3e9d0bddd4e829feaae08322ef59b4f494
Dreamwalker <aristojeff@gmail.com>
2022-06-07 19:50:34 +0900

    Create cosmetics_detail_page.dart

---
+++ lib/ep1174_cosmetics_app/src/ui/cosmetics_detail_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class CosmeticsDetailPage extends StatefulWidget {
+  const CosmeticsDetailPage({Key? key}) : super(key: key);
+
+  @override
+  State<CosmeticsDetailPage> createState() => _CosmeticsDetailPageState();
+}
+
+class _CosmeticsDetailPageState extends State<CosmeticsDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
