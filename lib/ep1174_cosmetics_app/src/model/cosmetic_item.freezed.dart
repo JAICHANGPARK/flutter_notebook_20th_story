1f54e0f8132990825b49c6b7239ed8878ceeb5fd
Dreamwalker <aristojeff@gmail.com>
2022-06-07 19:58:25 +0900

    Create cosmetic_item.freezed.dart

---
+++ lib/ep1174_cosmetics_app/src/model/cosmetic_item.freezed.dart
@@ -0,0 +1,311 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'cosmetic_item.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$CosmeticItem {
+  String? get title => throw _privateConstructorUsedError;
+  String? get review => throw _privateConstructorUsedError;
+  List<String>? get images => throw _privateConstructorUsedError;
+  String? get price => throw _privateConstructorUsedError;
+  String? get detail => throw _privateConstructorUsedError;
+  String? get useType => throw _privateConstructorUsedError;
+  String? get scent => throw _privateConstructorUsedError;
+  String? get liquidVolume => throw _privateConstructorUsedError;
+  String? get ml => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $CosmeticItemCopyWith<CosmeticItem> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $CosmeticItemCopyWith<$Res> {
+  factory $CosmeticItemCopyWith(
+          CosmeticItem value, $Res Function(CosmeticItem) then) =
+      _$CosmeticItemCopyWithImpl<$Res>;
+  $Res call(
+      {String? title,
+      String? review,
+      List<String>? images,
+      String? price,
+      String? detail,
+      String? useType,
+      String? scent,
+      String? liquidVolume,
+      String? ml});
+}
+
+/// @nodoc
+class _$CosmeticItemCopyWithImpl<$Res> implements $CosmeticItemCopyWith<$Res> {
+  _$CosmeticItemCopyWithImpl(this._value, this._then);
+
+  final CosmeticItem _value;
+  // ignore: unused_field
+  final $Res Function(CosmeticItem) _then;
+
+  @override
+  $Res call({
+    Object? title = freezed,
+    Object? review = freezed,
+    Object? images = freezed,
+    Object? price = freezed,
+    Object? detail = freezed,
+    Object? useType = freezed,
+    Object? scent = freezed,
+    Object? liquidVolume = freezed,
+    Object? ml = freezed,
+  }) {
+    return _then(_value.copyWith(
+      title: title == freezed
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String?,
+      review: review == freezed
+          ? _value.review
+          : review // ignore: cast_nullable_to_non_nullable
+              as String?,
+      images: images == freezed
+          ? _value.images
+          : images // ignore: cast_nullable_to_non_nullable
+              as List<String>?,
+      price: price == freezed
+          ? _value.price
+          : price // ignore: cast_nullable_to_non_nullable
+              as String?,
+      detail: detail == freezed
+          ? _value.detail
+          : detail // ignore: cast_nullable_to_non_nullable
+              as String?,
+      useType: useType == freezed
+          ? _value.useType
+          : useType // ignore: cast_nullable_to_non_nullable
+              as String?,
+      scent: scent == freezed
+          ? _value.scent
+          : scent // ignore: cast_nullable_to_non_nullable
+              as String?,
+      liquidVolume: liquidVolume == freezed
+          ? _value.liquidVolume
+          : liquidVolume // ignore: cast_nullable_to_non_nullable
+              as String?,
+      ml: ml == freezed
+          ? _value.ml
+          : ml // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$$_CosmeticItemCopyWith<$Res>
+    implements $CosmeticItemCopyWith<$Res> {
+  factory _$$_CosmeticItemCopyWith(
+          _$_CosmeticItem value, $Res Function(_$_CosmeticItem) then) =
+      __$$_CosmeticItemCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {String? title,
+      String? review,
+      List<String>? images,
+      String? price,
+      String? detail,
+      String? useType,
+      String? scent,
+      String? liquidVolume,
+      String? ml});
+}
+
+/// @nodoc
+class __$$_CosmeticItemCopyWithImpl<$Res>
+    extends _$CosmeticItemCopyWithImpl<$Res>
+    implements _$$_CosmeticItemCopyWith<$Res> {
+  __$$_CosmeticItemCopyWithImpl(
+      _$_CosmeticItem _value, $Res Function(_$_CosmeticItem) _then)
+      : super(_value, (v) => _then(v as _$_CosmeticItem));
+
+  @override
+  _$_CosmeticItem get _value => super._value as _$_CosmeticItem;
+
+  @override
+  $Res call({
+    Object? title = freezed,
+    Object? review = freezed,
+    Object? images = freezed,
+    Object? price = freezed,
+    Object? detail = freezed,
+    Object? useType = freezed,
+    Object? scent = freezed,
+    Object? liquidVolume = freezed,
+    Object? ml = freezed,
+  }) {
+    return _then(_$_CosmeticItem(
+      title: title == freezed
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String?,
+      review: review == freezed
+          ? _value.review
+          : review // ignore: cast_nullable_to_non_nullable
+              as String?,
+      images: images == freezed
+          ? _value._images
+          : images // ignore: cast_nullable_to_non_nullable
+              as List<String>?,
+      price: price == freezed
+          ? _value.price
+          : price // ignore: cast_nullable_to_non_nullable
+              as String?,
+      detail: detail == freezed
+          ? _value.detail
+          : detail // ignore: cast_nullable_to_non_nullable
+              as String?,
+      useType: useType == freezed
+          ? _value.useType
+          : useType // ignore: cast_nullable_to_non_nullable
+              as String?,
+      scent: scent == freezed
+          ? _value.scent
+          : scent // ignore: cast_nullable_to_non_nullable
+              as String?,
+      liquidVolume: liquidVolume == freezed
+          ? _value.liquidVolume
+          : liquidVolume // ignore: cast_nullable_to_non_nullable
+              as String?,
+      ml: ml == freezed
+          ? _value.ml
+          : ml // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_CosmeticItem implements _CosmeticItem {
+  _$_CosmeticItem(
+      {this.title,
+      this.review,
+      final List<String>? images,
+      this.price,
+      this.detail,
+      this.useType,
+      this.scent,
+      this.liquidVolume,
+      this.ml})
+      : _images = images;
+
+  @override
+  final String? title;
+  @override
+  final String? review;
+  final List<String>? _images;
+  @override
+  List<String>? get images {
+    final value = _images;
+    if (value == null) return null;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(value);
+  }
+
+  @override
+  final String? price;
+  @override
+  final String? detail;
+  @override
+  final String? useType;
+  @override
+  final String? scent;
+  @override
+  final String? liquidVolume;
+  @override
+  final String? ml;
+
+  @override
+  String toString() {
+    return 'CosmeticItem(title: $title, review: $review, images: $images, price: $price, detail: $detail, useType: $useType, scent: $scent, liquidVolume: $liquidVolume, ml: $ml)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_CosmeticItem &&
+            const DeepCollectionEquality().equals(other.title, title) &&
+            const DeepCollectionEquality().equals(other.review, review) &&
+            const DeepCollectionEquality().equals(other._images, _images) &&
+            const DeepCollectionEquality().equals(other.price, price) &&
+            const DeepCollectionEquality().equals(other.detail, detail) &&
+            const DeepCollectionEquality().equals(other.useType, useType) &&
+            const DeepCollectionEquality().equals(other.scent, scent) &&
+            const DeepCollectionEquality()
+                .equals(other.liquidVolume, liquidVolume) &&
+            const DeepCollectionEquality().equals(other.ml, ml));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      const DeepCollectionEquality().hash(title),
+      const DeepCollectionEquality().hash(review),
+      const DeepCollectionEquality().hash(_images),
+      const DeepCollectionEquality().hash(price),
+      const DeepCollectionEquality().hash(detail),
+      const DeepCollectionEquality().hash(useType),
+      const DeepCollectionEquality().hash(scent),
+      const DeepCollectionEquality().hash(liquidVolume),
+      const DeepCollectionEquality().hash(ml));
+
+  @JsonKey(ignore: true)
+  @override
+  _$$_CosmeticItemCopyWith<_$_CosmeticItem> get copyWith =>
+      __$$_CosmeticItemCopyWithImpl<_$_CosmeticItem>(this, _$identity);
+}
+
+abstract class _CosmeticItem implements CosmeticItem {
+  factory _CosmeticItem(
+      {final String? title,
+      final String? review,
+      final List<String>? images,
+      final String? price,
+      final String? detail,
+      final String? useType,
+      final String? scent,
+      final String? liquidVolume,
+      final String? ml}) = _$_CosmeticItem;
+
+  @override
+  String? get title => throw _privateConstructorUsedError;
+  @override
+  String? get review => throw _privateConstructorUsedError;
+  @override
+  List<String>? get images => throw _privateConstructorUsedError;
+  @override
+  String? get price => throw _privateConstructorUsedError;
+  @override
+  String? get detail => throw _privateConstructorUsedError;
+  @override
+  String? get useType => throw _privateConstructorUsedError;
+  @override
+  String? get scent => throw _privateConstructorUsedError;
+  @override
+  String? get liquidVolume => throw _privateConstructorUsedError;
+  @override
+  String? get ml => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$$_CosmeticItemCopyWith<_$_CosmeticItem> get copyWith =>
+      throw _privateConstructorUsedError;
+}
