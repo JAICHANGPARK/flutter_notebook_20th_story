67bf4b191497fbd8bda4591436466d08b5cd8888
Dreamwalker <aristojeff@gmail.com>
2022-06-08 20:24:00 +0900

    Update cosmetics_cart_page.dart

--- lib/ep1174_cosmetics_app/src/ui/cosmetics_cart_page.dart
+++ lib/ep1174_cosmetics_app/src/ui/cosmetics_cart_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class CosmeticsCartPage extends StatefulWidget {
   const CosmeticsCartPage({Key? key}) : super(key: key);
 
@@ -11,6 +10,23 @@ class CosmeticsCartPage extends StatefulWidget {
 class _CosmeticsCartPageState extends State<CosmeticsCartPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 0,
+        title: Text("My Cart"),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.shopping_basket_outlined,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
