fb11cea8a620bb67071cc8d7313e08b9bba86ffc
Dreamwalker <aristojeff@gmail.com>
2022-06-09 20:11:01 +0900

    Update cosmetic_cart_provider.dart

--- lib/ep1174_cosmetics_app/src/riverpod/cosmetic_cart_provider.dart
+++ lib/ep1174_cosmetics_app/src/riverpod/cosmetic_cart_provider.dart
@@ -1,9 +1,15 @@
 import 'package:flutter_notebook_20th_story/ep1174_cosmetics_app/src/model/cosmetic_item.dart';
+import 'package:flutter_notebook_20th_story/ep1174_cosmetics_app/src/ui/cosmetic_woman_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final cosmeticCartProvider = StateNotifierProvider((ref) => CosmeticCartController([]));
+final cosmeticCartProvider = StateNotifierProvider<CosmeticCartController, List<CosmeticItem>>(
+  (ref) => CosmeticCartController(
+    [
+      ...cosItems,
+    ],
+  ),
+);
 
 class CosmeticCartController extends StateNotifier<List<CosmeticItem>> {
   CosmeticCartController(super.state);
-
 }
