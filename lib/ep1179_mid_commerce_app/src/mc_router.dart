cd11483af10590e8b63ee413e3a2a3a4c2443d8c
Dreamwalker <aristojeff@gmail.com>
2022-06-12 18:07:08 +0900

    Update mc_router.dart

--- lib/ep1179_mid_commerce_app/src/mc_router.dart
+++ lib/ep1179_mid_commerce_app/src/mc_router.dart
@@ -1,13 +1,17 @@
 import 'package:flutter_notebook_20th_story/ep1179_mid_commerce_app/src/ui/mid_commerce_home_page.dart';
+import 'package:flutter_notebook_20th_story/ep1179_mid_commerce_app/src/ui/mid_commerce_info_page.dart';
 import 'package:go_router/go_router.dart';
 
 final mcRouter = GoRouter(
-  initialLocation: "/",
-  routes: [
-    GoRoute(
-      path: "/",
-      builder: (context, state) => const MidCommerceHomePage(),
-    )
-  ],
-  debugLogDiagnostics: true
-);
+    initialLocation: "/",
+    routes: [
+      GoRoute(
+        path: "/",
+        builder: (context, state) => const MidCommerceHomePage(),
+      ),
+      GoRoute(
+        path: "/info",
+        builder: (context, state) => const MidCommerceInfoPage(),
+      )
+    ],
+    debugLogDiagnostics: true);
