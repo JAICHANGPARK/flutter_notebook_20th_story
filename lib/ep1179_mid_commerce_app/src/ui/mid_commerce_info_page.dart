8c028cfd2a90dfac8df56330dcdc6622ccaea6bf
Dreamwalker <aristojeff@gmail.com>
2022-06-12 18:07:11 +0900

    Create mid_commerce_info_page.dart

---
+++ lib/ep1179_mid_commerce_app/src/ui/mid_commerce_info_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class MidCommerceInfoPage extends StatefulWidget {
+  const MidCommerceInfoPage({Key? key}) : super(key: key);
+
+  @override
+  State<MidCommerceInfoPage> createState() => _MidCommerceInfoPageState();
+}
+
+class _MidCommerceInfoPageState extends State<MidCommerceInfoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
