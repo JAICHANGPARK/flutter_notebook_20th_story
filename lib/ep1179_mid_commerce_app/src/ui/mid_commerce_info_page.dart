d984fa6ca4e6644a34e24cf3ae4a0d3d1dac971b
Dreamwalker <aristojeff@gmail.com>
2022-06-12 18:08:00 +0900

    Update mid_commerce_info_page.dart

--- lib/ep1179_mid_commerce_app/src/ui/mid_commerce_info_page.dart
+++ lib/ep1179_mid_commerce_app/src/ui/mid_commerce_info_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class MidCommerceInfoPage extends StatefulWidget {
   const MidCommerceInfoPage({Key? key}) : super(key: key);
 
@@ -12,7 +11,13 @@ class _MidCommerceInfoPageState extends State<MidCommerceInfoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+      ),
+      body: Column(
+        children: [],
+      ),
     );
   }
 }
