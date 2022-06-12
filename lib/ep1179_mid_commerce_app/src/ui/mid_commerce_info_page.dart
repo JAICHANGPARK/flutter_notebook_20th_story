d23fcb41fa1d6ad66bd1e97cd1aa86746e7842c5
Dreamwalker <aristojeff@gmail.com>
2022-06-12 18:11:54 +0900

    Update mid_commerce_info_page.dart

--- lib/ep1179_mid_commerce_app/src/ui/mid_commerce_info_page.dart
+++ lib/ep1179_mid_commerce_app/src/ui/mid_commerce_info_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class MidCommerceInfoPage extends StatefulWidget {
   const MidCommerceInfoPage({Key? key}) : super(key: key);
@@ -14,12 +15,40 @@ class _MidCommerceInfoPageState extends State<MidCommerceInfoPage> {
       body: Column(
         children: [
           Container(
-            decoration: BoxDecoration(
-              color: Colors.grey[100]
-            ),
+            decoration: BoxDecoration(color: Colors.grey[100]),
+            padding: EdgeInsets.fromLTRB(16, 48, 16, 16),
             child: Column(
               children: [
-
+                IconButton(
+                  onPressed: () {
+                    context.pop();
+                  },
+                  icon: Icon(Icons.arrow_back),
+                ),
+                Row(
+                  children: [
+                    CircleAvatar(),
+                    Column(
+                      children: [
+                        Text("Mid-Commerce"),
+                        Text("John"),
+                      ],
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Join",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                )
               ],
             ),
           )
