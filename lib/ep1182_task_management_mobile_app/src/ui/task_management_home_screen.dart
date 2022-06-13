bbc9009e872c84a23a681b81ff24ae940846905a
Dreamwalker <aristojeff@gmail.com>
2022-06-13 19:59:34 +0900

    Update task_management_home_screen.dart

--- lib/ep1182_task_management_mobile_app/src/ui/task_management_home_screen.dart
+++ lib/ep1182_task_management_mobile_app/src/ui/task_management_home_screen.dart
@@ -8,45 +8,55 @@ class TaskManagementHomeScreen extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
-              padding: const EdgeInsets.fromLTRB(16, 4, 16, 16),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              padding: const EdgeInsets.fromLTRB(16, 4, 16, 32),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Container(
-                    height: 42,
-                    width: 42,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.grey,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        height: 42,
+                        width: 42,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.grey,
+                          ),
+                          shape: BoxShape.circle,
+                        ),
+                        child: Icon(
+                          Icons.apps,
+                          color: Colors.blue,
+                        ),
                       ),
-                      shape: BoxShape.circle,
-                    ),
-                    child: Icon(
-                      Icons.apps,
-                      color: Colors.blue,
-                    ),
+                      Container(
+                        height: 42,
+                        width: 42,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.grey,
+                          ),
+                          shape: BoxShape.circle,
+                        ),
+                        child: Icon(
+                          Icons.notifications_none,
+                          color: Colors.grey,
+                        ),
+                      )
+                    ],
                   ),
-                  Container(
-                    height: 42,
-                    width: 42,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.grey,
-                      ),
-                      shape: BoxShape.circle,
-                    ),
-                    child: Icon(
-                      Icons.notifications_none,
-                      color: Colors.grey,
-                    ),
-                  )
+                  SizedBox(height: 24,),
+                  Text("Hey, Dream Walker"),
+                  Text("Are you ready to get back to your workplace?"),
+                  Divider(
+                    color: Colors.grey,
+                  ),
                 ],
               ),
             ),
-            Text("Hey, Dream Walker"),
-            Text("Are you ready to get back to your workplace?")
           ],
         ),
       ),
