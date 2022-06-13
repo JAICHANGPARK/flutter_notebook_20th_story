46b563490efd8cf1e77b49284d84d0a83a13c218
Dreamwalker <aristojeff@gmail.com>
2022-06-13 19:53:18 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_20th_story/ep1179_mid_commerce_app/mid_commerce_app.dart';
+import 'package:flutter_notebook_20th_story/ep1182_task_management_mobile_app/task_management_mobile_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 main() {
   runApp(
     ProviderScope(
-      child: MidCommerceApp(),
+      child: TaskManagementMobileApp(),
     ),
   );
 }
