4a71cf01e665c420f2a38c329101cf7d190a307a
Dreamwalker <aristojeff@gmail.com>
2022-06-13 19:56:44 +0900

    Update task_management_home_screen.dart

--- lib/ep1182_task_management_mobile_app/src/ui/task_management_home_screen.dart
+++ lib/ep1182_task_management_mobile_app/src/ui/task_management_home_screen.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
 
-
 class TaskManagementHomeScreen extends StatelessWidget {
   const TaskManagementHomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16, 32, 16, 16,),
+            child: Row(
+              children: [
+                Container(
+                  height: 58,
+                  width: 58,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey,
+                    ),
+                    shape: BoxShape.circle,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
