1d06dbb76f8490d64326e9d37c20eabbb6dce510
Dreamwalker <aristojeff@gmail.com>
2022-06-13 19:52:23 +0900

    Create task_management_mobile_app.dart

---
+++ lib/ep1182_task_management_mobile_app/task_management_mobile_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class TaskManagementMobileApp extends StatelessWidget {
+  const TaskManagementMobileApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
