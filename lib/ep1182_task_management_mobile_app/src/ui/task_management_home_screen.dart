af1b296cf48f901509d5568d4937ec3820cb6dc5
Dreamwalker <aristojeff@gmail.com>
2022-06-13 19:54:54 +0900

    Create task_management_home_screen.dart

---
+++ lib/ep1182_task_management_mobile_app/src/ui/task_management_home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class TaskManagementHomeScreen extends StatelessWidget {
+  const TaskManagementHomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
