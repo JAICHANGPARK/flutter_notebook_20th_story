ac35fe05577d0399878c3475c173497901d477d3
Dreamwalker <aristojeff@gmail.com>
2022-06-13 19:54:50 +0900

    Update task_management_mobile_app.dart

--- lib/ep1182_task_management_mobile_app/task_management_mobile_app.dart
+++ lib/ep1182_task_management_mobile_app/task_management_mobile_app.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_notebook_20th_story/ep1182_task_management_mobile_app/src/ui/task_management_home_screen.dart';
 
 class TaskManagementMobileApp extends StatelessWidget {
   const TaskManagementMobileApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      initialRoute: "/",
+      routes: {
+        "/": (context) => TaskManagementHomeScreen(),
+      },
+    );
   }
 }
