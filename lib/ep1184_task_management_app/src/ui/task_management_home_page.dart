958516bc059839a86db3fa3710590a530b90c319
Dreamwalker <aristojeff@gmail.com>
2022-06-15 23:12:45 +0900

    Update task_management_home_page.dart

--- lib/ep1184_task_management_app/src/ui/task_management_home_page.dart
+++ lib/ep1184_task_management_app/src/ui/task_management_home_page.dart
@@ -1,6 +1,4 @@
-
 import 'package:flutter/material.dart';
-
 
 class TaskManagementHomePage extends StatefulWidget {
   const TaskManagementHomePage({Key? key}) : super(key: key);
@@ -12,6 +10,37 @@ class TaskManagementHomePage extends StatefulWidget {
 class _TaskManagementHomePageState extends State<TaskManagementHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [],
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              height: 64,
+              decoration: BoxDecoration(color: Color.fromRGBO(44, 46, 47, 1)),
+              child: Row(
+                children: [
+                  Column(
+                    children: [
+                      Icon(Icons.home_outlined),
+                      Container(
+                        color: Colors.orange,
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
