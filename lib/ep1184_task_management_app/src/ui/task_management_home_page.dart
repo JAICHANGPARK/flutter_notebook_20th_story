a1cf7e473416cf00a5364544a80c0e70603e9a57
Dreamwalker <aristojeff@gmail.com>
2022-06-15 23:14:41 +0900

    Update task_management_home_page.dart

--- lib/ep1184_task_management_app/src/ui/task_management_home_page.dart
+++ lib/ep1184_task_management_app/src/ui/task_management_home_page.dart
@@ -14,24 +14,37 @@ class _TaskManagementHomePageState extends State<TaskManagementHomePage> {
       backgroundColor: Colors.black,
       body: Stack(
         children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [],
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            top: 0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [],
+            ),
           ),
           Positioned(
             left: 0,
             right: 0,
             bottom: 0,
             child: Container(
-              height: 64,
+              height: 52,
               decoration: BoxDecoration(color: Color.fromRGBO(44, 46, 47, 1)),
+              padding: EdgeInsets.only(top: 16, right: 24, left: 24),
               child: Row(
                 children: [
                   Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Icon(Icons.home_outlined),
+                      Icon(
+                        Icons.home_outlined,
+                        color: Colors.white,
+                      ),
                       Container(
                         color: Colors.orange,
+                        height: 4,
+                        width: 16,
                       )
                     ],
                   )
