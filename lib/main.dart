d342a9e453e7e2e2240cf3716748237c73b781e5
Dreamwalker <aristojeff@gmail.com>
2022-06-20 21:20:19 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_20th_story/ep1184_task_management_app/task_management_app.dart';
+import 'package:flutter_notebook_20th_story/ep1188_sabel_app/sabel_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 main() {
   runApp(
     ProviderScope(
-      child: TaskManagementApp(),
+      child: SabelApp(),
     ),
   );
 }
