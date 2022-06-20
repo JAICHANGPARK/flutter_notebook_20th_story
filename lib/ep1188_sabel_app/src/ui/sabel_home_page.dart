3a2d770bddb1ecd17c850621a53d481027b04c2b
Dreamwalker <aristojeff@gmail.com>
2022-06-20 21:22:30 +0900

    Update sabel_home_page.dart

--- lib/ep1188_sabel_app/src/ui/sabel_home_page.dart
+++ lib/ep1188_sabel_app/src/ui/sabel_home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class SabelHomePage extends StatefulWidget {
   const SabelHomePage({Key? key}) : super(key: key);
 
@@ -12,13 +11,37 @@ class _SabelHomePageState extends State<SabelHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-          child: Stack(
-            children: [
-              
-            ],
-          ),
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [],
+                ),
+              ),
+            ),
+            Positioned(
+              left: 64,
+              right: 64,
+              bottom: 24,
+              child: Container(
+                height: 64,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(24),
+                ),
+                child: Row(
+                  children: [
+                    
+                  ],
+                ),
+              ),
+            )
+          ],
         ),
+      ),
     );
   }
 }
