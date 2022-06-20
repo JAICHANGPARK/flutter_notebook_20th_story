93e2cee7a9de056a84ed934767c2c475b4fa3e66
Dreamwalker <aristojeff@gmail.com>
2022-06-20 21:20:05 +0900

    Update sabel_app.dart

---
+++ lib/ep1188_sabel_app/sabel_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_notebook_20th_story/ep1188_sabel_app/src/ui/sabel_home_page.dart';
+
+class SabelApp extends StatelessWidget {
+  const SabelApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SabelHomePage(),
+    );
+  }
+}
