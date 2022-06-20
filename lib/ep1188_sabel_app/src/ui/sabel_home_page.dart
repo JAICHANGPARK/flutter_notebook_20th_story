930c4e4fc76c76b4158028387ae95e3ccfc47813
Dreamwalker <aristojeff@gmail.com>
2022-06-20 21:20:12 +0900

    Create sabel_home_page.dart

---
+++ lib/ep1188_sabel_app/src/ui/sabel_home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class SabelHomePage extends StatefulWidget {
+  const SabelHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<SabelHomePage> createState() => _SabelHomePageState();
+}
+
+class _SabelHomePageState extends State<SabelHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
