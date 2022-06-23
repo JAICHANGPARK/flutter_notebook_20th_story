581e4253fefe6d491678494d43ed76192af81704
Dreamwalker <aristojeff@gmail.com>
2022-06-23 22:20:52 +0900

    Create salmage_app.dart

---
+++ lib/ep1191_salmage_app/salmage_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class SalmageApp extends StatelessWidget {
+  const SalmageApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
