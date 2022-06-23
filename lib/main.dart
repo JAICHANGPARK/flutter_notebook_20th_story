80a43d475c0a9ee099bde0cd0638c61986c58ddd
Dreamwalker <aristojeff@gmail.com>
2022-06-23 22:21:36 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_20th_story/ep1188_sabel_app/sabel_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_notebook_20th_story/ep1191_salmage_app/salmage_app.dart';
 
 main() {
   runApp(
     ProviderScope(
-      child: SabelApp(),
+      child: SalmageApp(),
     ),
   );
 }
