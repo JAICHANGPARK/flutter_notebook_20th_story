d02dbec11ab3ad45fd55ef0f30d3453ed87e3401
Dreamwalker <aristojeff@gmail.com>
2022-06-23 22:23:16 +0900

    Create salmage_home_page.dart

---
+++ lib/ep1191_salmage_app/src/ui/salmage_home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SalmageHomePage extends StatefulWidget {
+  const SalmageHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<SalmageHomePage> createState() => _SalmageHomePageState();
+}
+
+class _SalmageHomePageState extends State<SalmageHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
