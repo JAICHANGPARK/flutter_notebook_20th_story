bb88207c1cd274e9dd70451c4ef3da3459e6664f
Dreamwalker <aristojeff@gmail.com>
2022-06-24 20:22:34 +0900

    Update salmage_products_screen.dart

--- lib/ep1191_salmage_app/src/ui/salmage_products_screen.dart
+++ lib/ep1191_salmage_app/src/ui/salmage_products_screen.dart
@@ -7,15 +7,35 @@ class SalmageProductScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Row(
-          children: [
-            CircleAvatar(
-              backgroundColor: Colors.white,
-              child: Icon(
-                Icons.arrow_back,
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Icon(
+                  Icons.arrow_back,
+                ),
               ),
-            ),
-          ],
+              Expanded(
+                  child: Center(
+                child: Text(
+                  "Products",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18
+                  ),
+                ),
+              )),
+              CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Icon(
+                  Icons.filter_alt_outlined,
+                ),
+              ),
+            ],
+          ),
         )
       ],
     );
