be78a58745c399cb36167472a8381d252f7311c0
Dreamwalker <aristojeff@gmail.com>
2022-06-24 20:10:00 +0900

    Create salmage_bottom_widget.dart

---
+++ lib/ep1191_salmage_app/src/ui/widgets/salmage_bottom_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class SalmageBottomWidget extends ConsumerWidget {
+  const SalmageBottomWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, ref) {
+    return Container();
+  }
+}
