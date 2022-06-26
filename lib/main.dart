25adb4e9b15e77f48d58f72824587ba71fb57947
Dreamwalker <aristojeff@gmail.com>
2022-06-26 23:01:03 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_20th_story/ep1194_logistic_mobile_app/logistic_mobile_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_notebook_20th_story/ep1191_salmage_app/salmage_app.dart';
 
 main() {
   runApp(
     ProviderScope(
-      child: SalmageApp(),
+      child: LogisticMobileApp(),
     ),
   );
 }
