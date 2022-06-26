59e46724633579f6ac6342373307a8c0f32d4c14
Dreamwalker <aristojeff@gmail.com>
2022-06-26 23:05:40 +0900

    Update logistic_login_page.dart

--- lib/ep1194_logistic_mobile_app/src/ui/logistic_login_page.dart
+++ lib/ep1194_logistic_mobile_app/src/ui/logistic_login_page.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
 
-
 class LogisticLoginPage extends StatelessWidget {
   const LogisticLoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          
-        ],
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Login",
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Text("Welcome back!"),
+            SizedBox(
+              height: 4,
+            ),
+            Text("Please enter your details"),
+          ],
+        ),
       ),
     );
   }
