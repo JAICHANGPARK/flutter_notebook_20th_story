9fe6b62d88f551447cbe0a1b7e88ab675b4ccfbd
Dreamwalker <aristojeff@gmail.com>
2022-06-26 22:59:25 +0900

    Create logistic_login_page.dart

---
+++ lib/ep1194_logistic_mobile_app/src/ui/logistic_login_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class LogisticLoginPage extends StatelessWidget {
+  const LogisticLoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
