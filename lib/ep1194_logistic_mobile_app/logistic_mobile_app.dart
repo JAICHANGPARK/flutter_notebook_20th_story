e4d966e68360d59e1a55c6465fe9f1e16e37b17f
Dreamwalker <aristojeff@gmail.com>
2022-06-26 22:59:19 +0900

    Update logistic_mobile_app.dart

---
+++ lib/ep1194_logistic_mobile_app/logistic_mobile_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_notebook_20th_story/ep1194_logistic_mobile_app/src/ui/logistic_login_page.dart';
+
+
+class LogisticMobileApp extends StatelessWidget {
+  const LogisticMobileApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LogisticLoginPage(),
+    );
+  }
+}
