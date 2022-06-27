9ead0fcbe455fca339773cced025b1fd983217a0
Dreamwalker <aristojeff@gmail.com>
2022-06-27 17:13:35 +0900

    Create logistic_home_page.dart

---
+++ lib/ep1194_logistic_mobile_app/src/ui/logistic_home_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class LogisticHomePage extends StatelessWidget {
+  const LogisticHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
