a6d1419cd7c63439676f914630f423f0153eda48
Dreamwalker <aristojeff@gmail.com>
2022-06-28 16:21:40 +0900

    Update logistic_home_page.dart

--- lib/ep1194_logistic_mobile_app/src/ui/logistic_home_page.dart
+++ lib/ep1194_logistic_mobile_app/src/ui/logistic_home_page.dart
@@ -8,31 +8,35 @@ class LogisticHomePage extends StatelessWidget {
     return Scaffold(
       bottomNavigationBar: BottomAppBar(
         child: Container(
-          height: 72,
-          child: Row(
-            children: [
-              Expanded(
-                  child: Column(
-                children: [
-                  Container(
-                    height: 3,
-                    color: Colors.black,
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.home_outlined,
+          height: 76,
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 8),
+            child: Row(
+              children: [
+                Expanded(
+                    child: Column(
+                  children: [
+                    Container(
+                      height: 3,
+                      color: Colors.black,
                     ),
-                  ),
-                  Text(
-                    "Home",
-                  )
-                ],
-              )),
-              Expanded(child: Placeholder()),
-              Expanded(child: Placeholder()),
-              Expanded(child: Placeholder()),
-            ],
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.home_outlined,
+                      ),
+                      iconSize: 32,
+                    ),
+                    Text(
+                      "Home",
+                    )
+                  ],
+                )),
+                Expanded(child: Placeholder()),
+                Expanded(child: Placeholder()),
+                Expanded(child: Placeholder()),
+              ],
+            ),
           ),
         ),
       ),
