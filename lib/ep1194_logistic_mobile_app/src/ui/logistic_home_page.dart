e6faef83f7d9a6b6934dc28a260f34a43d1a6fee
Dreamwalker <aristojeff@gmail.com>
2022-06-29 16:07:02 +0900

    Update logistic_home_page.dart

--- lib/ep1194_logistic_mobile_app/src/ui/logistic_home_page.dart
+++ lib/ep1194_logistic_mobile_app/src/ui/logistic_home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_20th_story/ep1194_logistic_mobile_app/src/ui/logistic_bottom_bar_widget.dart';
 import 'package:flutter_notebook_20th_story/ep1194_logistic_mobile_app/src/ui/logistic_filter_widget.dart';
+import 'package:flutter_notebook_20th_story/ep1194_logistic_mobile_app/src/ui/logistic_top_bar_widget.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class LogisticHomePage extends StatelessWidget {
@@ -12,43 +13,7 @@ class LogisticHomePage extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 16, right: 8, bottom: 16, top: 16),
-              child: Row(
-                children: [
-                  Text(
-                    "Filter",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(width: 8),
-                  CircleAvatar(
-                    radius: 6,
-                    backgroundColor: Colors.black,
-                  ),
-                  Spacer(),
-                  Consumer(builder: (context, ref, _) {
-                    final open = ref.watch(isFilterOpen);
-                    return Container(
-                      decoration: BoxDecoration(
-                        color: open ? Colors.white : Colors.transparent,
-                        shape: BoxShape.circle,
-                      ),
-                      child: IconButton(
-                        onPressed: () {
-                          ref.read(isFilterOpen.notifier).state = !open;
-                        },
-                        icon: Icon(
-                          Icons.filter_list,
-                        ),
-                      ),
-                    );
-                  })
-                ],
-              ),
-            ),
+            LogisticTopBarWidget(),
             LogisticFilterWidget(),
             Expanded(
               child: Container(
