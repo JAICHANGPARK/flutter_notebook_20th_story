0ab84a7020b3fa36fb56b74f371942c48462f429
Dreamwalker <aristojeff@gmail.com>
2022-07-01 13:34:59 +0900

    Create checkout_address_screen.dart

---
+++ lib/ep1199_e_commerce_checkout/src/ui/checkout_address_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CheckoutAddressScreen extends StatefulWidget {
+  const CheckoutAddressScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CheckoutAddressScreen> createState() => _CheckoutAddressScreenState();
+}
+
+class _CheckoutAddressScreenState extends State<CheckoutAddressScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
