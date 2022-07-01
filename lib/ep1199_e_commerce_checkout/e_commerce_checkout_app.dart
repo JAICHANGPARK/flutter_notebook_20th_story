374bf2c544f087b35eece8354613b1f9fbe59226
Dreamwalker <aristojeff@gmail.com>
2022-07-01 13:34:54 +0900

    Update e_commerce_checkout_app.dart

---
+++ lib/ep1199_e_commerce_checkout/e_commerce_checkout_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class ECommerceCheckoutApp extends StatelessWidget {
+  const ECommerceCheckoutApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
