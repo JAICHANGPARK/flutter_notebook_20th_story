bbf155916f5cdf5eb431163d9a5cdfb22b59ef0e
Dreamwalker <aristojeff@gmail.com>
2022-07-02 18:23:11 +0900

    Create checkout_payment_widget.dart

---
+++ lib/ep1199_e_commerce_checkout/src/ui/checkout_payment_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CheckoutPaymentWidget extends StatefulWidget {
+  const CheckoutPaymentWidget({Key? key}) : super(key: key);
+
+  @override
+  State<CheckoutPaymentWidget> createState() => _CheckoutPaymentWidgetState();
+}
+
+class _CheckoutPaymentWidgetState extends State<CheckoutPaymentWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+
+    );
+  }
+}
